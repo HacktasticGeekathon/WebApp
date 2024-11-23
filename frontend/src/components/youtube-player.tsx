bbd7e9ca4f0735757d825420a7994a87a00d29c5
MiguelR0drigues <miguel.rodrigues8@hotmail.com>
2024-11-23 15:07:55 +0000

    Initial commit with captions and markers (hardcoded)

---
+++ frontend/src/components/youtube-player.tsx
@@ -0,0 +1,54 @@
+import React, { useRef } from "react";
+import Plyr, { APITypes, PlyrOptions, PlyrSource } from "plyr-react";
+import "plyr-react/plyr.css";
+import Video from "../assets/trump.mp4";
+import captions from "../assets/captions.vtt";
+
+const VideoPlayer: React.FC = () => {
+  const plyrRef = useRef<APITypes>(null);
+
+  const videoSource: PlyrSource = {
+    type: "video" as const,
+    sources: [
+      {
+        src: Video,
+        type: "video/mp4",
+      },
+    ],
+    tracks: [
+      {
+        kind: "subtitles",
+        label: "English",
+        srcLang: "en",
+        src: captions,
+        default: true,
+      },
+    ],
+  };
+
+  const options: PlyrOptions = {
+    controls: ["play", "progress", "volume", "fullscreen", "settings"],
+    captions: { active: true, language: "en" },
+    markers: {
+      enabled: true,
+      points: [
+        {
+          time: 5,
+          label: "Test marker",
+        },
+        {
+          time: 15,
+          label: "One minute",
+        },
+      ],
+    },
+  };
+
+  return (
+    <div style={{ maxWidth: "800px", margin: "0 auto" }}>
+      <Plyr ref={plyrRef} source={videoSource} options={options} />
+    </div>
+  );
+};
+
+export default VideoPlayer;
