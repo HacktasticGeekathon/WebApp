b2de9a97306dc41253e7aed5b69054c5306f23b2
José Coelho <16445494+jcoelho93@users.noreply.github.com>
2024-11-24 10:35:21 +0000

    Dockerize it

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,10 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   assetsInclude: ["**/*.mp4", "**/*.vtt"],
+  server: {
+    port: 3000
+  },
+  preview: {
+    port: 3000
+  }
 });
